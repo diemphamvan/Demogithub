e0d23ed6c4b8a4109a9b461cacfcdbff1098665d
diemphamvan <diemphamvan.uneti@gmail.com>
2018-04-22 10:24:05 +0700

    update lan dau

--- app/src/main/java/com/example/my/demogithub/MainActivity.java
+++ app/src/main/java/com/example/my/demogithub/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    int a;
+    String b;
+    Float c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
